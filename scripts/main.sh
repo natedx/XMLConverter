648c753e9753c2ecf2f3d9607181dd8bbd9176a8
Nathan Daix <46789008+natedx@users.noreply.github.com>
2026-01-21 22:18:35 +0100

    Adding comments

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,12 +1,22 @@
+echo "Starting XML conversion"
+
 python3 parseToValidXML.py
+
+echo "Finished cleaning XML file"
 
 xmllint --noout code_penal_fixed.xml
 #xmllint --recover --noent code_penal_fixed.xml > code_penal_fixed.xml
 
+echo "Verified that XML file is clean"
+
 xsltproc stylesheet.xsl code_penal_fixed.xml > output.html
+
+echo "Using stylesheet to translate XML to HTML"
 
 pandoc output.html \
   --from=html \
   --to=docx \
   --standalone \
   -o output.docx
+
+echo "Finished XML conversion"
