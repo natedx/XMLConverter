c22ef78ad4921c99e97df2dce156d45a1861bcd3
Nathan Daix <46789008+natedx@users.noreply.github.com>
2026-01-21 22:08:47 +0100

    Working version of the script.

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,4 +3,4 @@ python3 parseToValidXML.py
 xmllint --noout code_penal_fixed.xml
 #xmllint --recover --noent code_penal_fixed.xml > code_penal_fixed.xml
 
-# xsltproc stylesheet.xsl code_penal.xml > output.html
+xsltproc stylesheet.xsl code_penal_fixed.xml > output.html
--- scripts/parseToValidXML.py
+++ scripts/parseToValidXML.py
@@ -1,10 +1,28 @@
-import html, sys
+import re
+
+# Entités XML à NE PAS toucher
+XML_BUILTINS = {"amp", "lt", "gt", "quot", "apos"}
 
 with open("code_penal.xml", "r", encoding="utf-8", errors="replace") as f:
     data = f.read()
 
-# convertit &eacute; &agrave; &mdash; etc en vrais caractères UTF-8
-data = html.unescape(data)
+# Convertit uniquement les entités nommées NON XML (ex: egrave, eacute...)
+def repl(m):
+    name = m.group(1)
+    if name in XML_BUILTINS:
+        return "&" + name + ";"   # on garde tel quel
+    # sinon on convertit en entité numérique décimale
+    # ex: &egrave; -> &#232;
+    import html
+    ch = html.unescape("&" + name + ";")
+    if ch.startswith("&"):  # entité inconnue
+        return "&" + name + ";"
+    return "&#" + str(ord(ch)) + ";"
+
+data = re.sub(r"&([A-Za-z][A-Za-z0-9]+);", repl, data)
+
+# Remove DOCTYPE declaration (proprietary DTD)
+data = re.sub(r'<!DOCTYPE[^>]*>', '', data)
 
 with open("code_penal_fixed.xml", "w", encoding="utf-8") as f:
     f.write(data)
