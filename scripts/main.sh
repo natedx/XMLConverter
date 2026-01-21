3342609124e5ffcb05b95ded017e09619d0fb617
Nathan Daix <46789008+natedx@users.noreply.github.com>
2026-01-21 22:14:26 +0100

    Working version of main script, which outputs a valid Word document.

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,3 +4,9 @@ xmllint --noout code_penal_fixed.xml
 #xmllint --recover --noent code_penal_fixed.xml > code_penal_fixed.xml
 
 xsltproc stylesheet.xsl code_penal_fixed.xml > output.html
+
+pandoc output.html \
+  --from=html \
+  --to=docx \
+  --standalone \
+  -o output.docx
