00ce7ddb7bef2b6240bf7698f449d5eced970d32
Nathan Daix <46789008+natedx@users.noreply.github.com>
2026-01-27 10:22:16 +0100

    Meilleurs commentaires (en français) du script main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,18 +1,22 @@
-echo "Starting XML conversion"
+echo "Démarrage de la conversion XML..."
+echo "Début du nettoyage du XML..."
 
 python3 parseToValidXML.py
 
-echo "Finished cleaning XML file"
+echo "Fin du nettoyage du XML"
+echo "Début de la vérification de l'intégrité du XML..."
 
 xmllint --noout code_penal_fixed.xml
-#xmllint --recover --noent code_penal_fixed.xml > code_penal_fixed.xml
 
-echo "Verified that XML file is clean"
+echo "Fin de la vérification de l'intégrité du XML"
+echo "Début de la transformation du XML en HTML, grâce au stylesheet XSLT..."
 
 xsltproc stylesheet.xsl code_penal_fixed.xml > output.html
 
-echo "Finished translating XML to HTML with stylesheet"
+echo "Fin de la transformation du XML en HTML, grâce au stylesheet"
+echo "Début de la transformation du HTML en .docx, avec Pandoc et reference.docx..."
 
 pandoc output.html -o output.docx --reference-doc=reference.docx
 
-echo "Finished XML conversion to .docx Word"
+echo "Fin de la transformation du HTML en .docx, avec Pandoc et reference.docx"
+echo "Fin de la conversion XML en .docx -> output.docx"
